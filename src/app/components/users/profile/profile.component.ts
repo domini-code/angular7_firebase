91c9eafc872dd6a8d112b26e2eca12a5b577e970
Bezael <bezael_e@hotmail.com>
2018-11-26 19:08:52 +0100

    :feat: profile user

--- src/app/components/users/profile/profile.component.ts
+++ src/app/components/users/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { UserInterface } from '../../../models/user';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private authService: AuthService) { }
+  user: UserInterface = {
+    name: '',
+    email: '',
+    photoUrl: ''
+  };
+  public providerId: string = 'null';
   ngOnInit() {
+    this.authService.isAuth().subscribe(user => {
+      if (user) {
+        this.user.name = user.displayName;
+        this.user.email = user.email;
+        this.user.photoUrl = user.photoURL;
+        this.providerId = user.providerData[0].providerId;
+      }
+    })
   }
 
 }
